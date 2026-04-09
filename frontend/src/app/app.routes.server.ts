43e2c8a1ae1ef1105912a3815f6bb23e14dfeecd
AntonioPascualMasMartinez <apmm2@gcloud.ua.es>
2026-04-09 18:40:32 +0200

    [fix] Rutas para el deployment

--- frontend/src/app/app.routes.server.ts
+++ frontend/src/app/app.routes.server.ts
@@ -1,8 +1,21 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  // 1. Definimos las rutas dinámicas para que NO intenten prerrenderizarse.
+  // Al usar RenderMode.Server, Angular las renderizará en el servidor cuando el usuario las pida.
+  {
+    path: 'home/proyectos/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'home/proyectos/:id/edit',
+    renderMode: RenderMode.Server,
+  },
+
+  // 2. Para el resto de rutas (Landing, Login, etc.), mantenemos el Prerender
+  // para que la carga inicial sea instantánea y mejore el SEO.
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
